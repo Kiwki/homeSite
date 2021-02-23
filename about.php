b82348c93a213960956dbef5c8fd49dc27929c04
kiwki <kapp.quentin67@gmail.com>
2021-02-23 09:33:43 +0100

    about page, top adjustement

--- about.php
+++ about.php
@@ -4,96 +4,30 @@
         <title>QK | Developper</title>
     	<link rel="icon" href="images/favicon.png" type="image/png" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script type='text/javascript' src='scriptJS/carousel.js'></script>
+		<script type='text/javascript' src='scriptJS/jquery.min.js'></script>
 		<script type='text/javascript' src='scriptJS/jquery.particleground.js'></script>
 		<script type='text/javascript' src='scriptJS/mainParticule.js'></script>
 		<link rel="stylesheet" href="styleCSS/mainStyle.css">
 		<style>
 		<style>
-			* {box-sizing: border-box;}
-			body {font-family: Verdana, sans-serif;}
-			.mySlides {display: none;}
-			img {vertical-align: middle;}
-			
 			#wrapper {
-				width: 100%;
-				height:100%;
-				border: none;
-				overflow: hidden; /* will contain if #first is longer than #second */
+				float: none;
+				width: 50%;
+				height: 50%;
+				overflow: scroll;
+				text-align: center;
+				display: flex;
+				flex-direction: column;
 			}
 			#first {
-				height: 30%;
-				float:left; /* add this */
-				border: none;
+				position: -webkit-sticky;
+				position: sticky;
+				top:0;
+				width: 100%;
 			}
 			#second {
-				border: none;
-				overflow: hidden; /* if you don't want #second to wrap below #first */
-			}
-			
-			/* Slideshow container */
-			.slideshow-container {
-			  max-width: 60%;
-			  position: relative;
-			  margin: 10px;
-			}
-
-			/* Caption text */
-			.text {
-			  color: #f2f2f2;
-			  font-size: 15px;
-			  padding: 8px 12px;
-			  position: absolute;
-			  bottom: 8px;
-			  width: 100%;
-			  text-align: center;
-			}
-
-			/* Number text (1/3 etc) */
-			.numbertext {
-			  color: #f2f2f2;
-			  font-size: 12px;
-			  padding: 8px 12px;
-			  position: absolute;
-			  top: 0;
-			}
-
-			/* The dots/bullets/indicators */
-			.dot {
-			  height: 15px;
-			  width: 15px;
-			  margin: 0 2px;
-			  background-color: #bbb;
-			  border-radius: 50%;
-			  display: inline-block;
-			  transition: background-color 0.6s ease;
-			}
-
-			.active {
-			  background-color: #717171;
-			}
-
-			/* Fading animation */
-			.fade {
-			  -webkit-animation-name: fade;
-			  -webkit-animation-duration: 1.5s;
-			  animation-name: fade;
-			  animation-duration: 1.5s;
-			}
-
-			@-webkit-keyframes fade {
-			  from {opacity: .4} 
-			  to {opacity: 1}
-			}
-
-			@keyframes fade {
-			  from {opacity: .4} 
-			  to {opacity: 1}
-			}
-
-			/* On smaller screens, decrease text size */
-			@media only screen and (max-width: 300px) {
-			  .text {font-size: 11px}
+				width: 100%;
+				text-align: center;
 			}
 		</style>
     </head>
@@ -106,81 +40,38 @@
 		</div>
 		<div id="particles">
 			<div id="intro">
-				<div id="wrapper">
-					<h2 id="first">Professionally connected with web/software/mobile development and programming technologies for many years.<br>
+				<center><div id="wrapper">
+					<h2 id="first">Professionally connected with web/software/mobile development for many years.<br>
 								   Well-organised person, problem solver, independent employee with high attention to detail.<br>
-								   Fan of outdoor activities, TV series and anime/manga.
+								   Fan of outdoor activities, TV series, anime/manga and so many others things.
 					</h2>
-					<div class="slideshow-container" id="second">
-						<div class="mySlides fade">
-							<div class="numbertext">1 / 6</div>
-								<img src="images/mangas.png" style="width:100%">
-								<div class="text">Mangas / Animes</div>
-						</div>
-
-						<div class="mySlides fade">
-							<div class="numbertext">2 / 6</div>
-								<img src="images/chess.png" style="width:100%">
-								<div class="text">Chess</div>
-						</div>
-
-						<div class="mySlides fade">
-							<div class="numbertext">3 / 6</div>
-								<img src="images/games.png" style="width:100%">
-								<div class="text">Video Games</div>
-						</div>
-								
-						<div class="mySlides fade">
-							<div class="numbertext">4 / 6</div>
-								<img src="images/nTechnologies.png" style="width:100%">
-								<div class="text">News Technologies</div>
-						</div>
-								
-						<div class="mySlides fade">
-							<div class="numbertext">5 / 6</div>
-								<img src="images/voyage.png" style="width:100%">
-								<div class="text">Travel</div>
-						</div>
-								
-						<div class="mySlides fade">
-							<div class="numbertext">6 / 6</div>
-								<img src="images/curiosite.png" style="width:100%">
-								<div class="text">Curious / thirsty for knowledge</div>
-						</div>
+					<div id="second">
+						<img src="images/mangas.png" width="10%" height="10%" id="hobby1"> 
+						<img src="images/games.png" width="10%" height="10%" id="hobby2"> 
+						<img src="images/chess.png" width="10%" height="10%" id="hobby3"> 
+						<img src="images/curiosite.png" width="10%" height="10%" id="hobby4"> 
+						<img src="images/nTechnologies.png" width="10%" height="10%" id="hobby5"> 
+						<img src="images/voyage.png" width="10%" height="10%" id="hobby6"> 
 					</div>
-					<br>
-
-					<div style="text-align:center">
-						<span class="dot"></span> 
-						<span class="dot"></span> 
-						<span class="dot"></span>
-						<span class="dot"></span>
-						<span class="dot"></span>
-						<span class="dot"></span>
-					</div>
-				</div>	
 				<script>
-					var slideIndex = 0;
-					showSlides();
+					$(function() {
+					   var ancien='';
+					   $("img").click(function(){
+						 var ddd="#"+$(this).attr("id");
+						 if(ancien!='' && ancien!=ddd)
+						 {
+						   // alert(ancien);
+						  $(ancien).animate({'height':'10%','width':'10%',},1000);
+						 }
+						 $(ddd).animate({'height':'40%','width':'40%',},1000);
+						 //$(ddd).fadeOut(3000);
 
-					function showSlides() {
-					  var i;
-					  var slides = document.getElementsByClassName("mySlides");
-					  var dots = document.getElementsByClassName("dot");
-					  for (i = 0; i < slides.length; i++) {
-						slides[i].style.display = "none";  
-					  }
-					  slideIndex++;
-					  if (slideIndex > slides.length) {slideIndex = 1}    
-					  for (i = 0; i < dots.length; i++) {
-						dots[i].className = dots[i].className.replace(" active", "");
-					  }
-					  slides[slideIndex-1].style.display = "block";  
-					  dots[slideIndex-1].className += " active";
-					  setTimeout(showSlides, 5000);
-					}
+						 ancien=ddd;
+					   });
+					 });
 				</script>
-			</div>
+				</div>	
+			</div></center>
 		</div>
    </body>
 </html>
